94c81ea186b7f82af3ffe81bfa94c87ccf48c32c
Lautaro Cardozo <velozpro450@gmail.com>
2025-08-20 14:39:19 -0300

    Cambios en Register

--- Register-Modal.js
+++ Register-Modal.js
@@ -1,6 +1,12 @@
-const boton_Profesores = document.getElementById("");
-const boton_Adscriptas = document.getElementById("");
-const boton_Secretaria = document.getElementById("");
-const boton_Salones = document.getElementById("");
-const boton_Recurosos = document.getElementById("");
+const boton_Profesores = document.getElementById("Profesores-boton");
+const boton_Adscriptas = document.getElementById("Adscripta-boton");
+const boton_Secretaria = document.getElementById("Secretaria-boton");
+const boton_Salones = document.getElementById("Salones-boton");
+const boton_Recurosos = document.getElementById("Recursos-boton");
 
+boton_Profesores.addEventListener("click", function() {
+    
+
+
+
+})
