0c1de36eca5ff0efc8c1c7e88977a4ffba2853d0
Lautaro Cardozo <velozpro450@gmail.com>
2025-08-20 09:28:55 -0300

    Camibios en pagina Registro
    Cambios en el color del fondo y estilos varios

---
+++ Register-Modal.js
@@ -0,0 +1,6 @@
+const boton_Profesores = document.getElementById("");
+const boton_Adscriptas = document.getElementById("");
+const boton_Secretaria = document.getElementById("");
+const boton_Salones = document.getElementById("");
+const boton_Recurosos = document.getElementById("");
+
