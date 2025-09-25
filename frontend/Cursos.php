0f709ea81fd5a360f68b008ebf427a9290abebe9
Lautaro Cardozo <velozpro450@gmail.com>
2025-09-25 13:25:12 -0300

    a

--- frontend/Cursos.php
+++ frontend/Cursos.php
@@ -23,64 +23,65 @@ $query = mysqli_query($connect, $sql);
 <body>
     <!-- trae las barras de navegacion (sidebar y superior) -->
     <?php include 'nav.php'; ?>
-
-    <div id="contenido-asignatura">
-        <h1>Cursos</h1>
-        <table id="datos">
-            <tr>
-                <th class="id">Id </th>
-                <th class="nombre-titulo">Nombre</th>
-                <th class="nombre-titulo">Capacidad</th>
-                <th class="titulo-ult">Cupos Disponibles</th>
-                <th class="boton-titulo">Borrar</th>
-                <th class="boton-titulo">Editar</th>
-            </tr>
-            <?php while ($row = mysqli_fetch_array($query)): ?>
+    <main>
+        <div id="contenido-asignatura">
+            <h1>Cursos</h1>
+            <table id="datos">
+                <tr>
+                    <th class="id">Id </th>
+                    <th class="nombre-titulo">Nombre</th>
+                    <th class="nombre-titulo">Capacidad</th>
+                    <th class="titulo-ult">Cupos Disponibles</th>
+                    <th class="boton-titulo">Borrar</th>
+                    <th class="boton-titulo">Editar</th>
+                </tr>
+                <?php while ($row = mysqli_fetch_array($query)): ?>
                     <tr class="mostrar-datos">
                         <th><?= $row['id_curso'] ?></th>
                         <th class="nombre"><?= $row['nombre'] ?></th>
                         <th class="nombre"><?= $row['capacidad'] ?></th>
                         <th class="ultimo-dato"><?= $row['cupo_disponible'] ?></th>
                         <th class="boton-dato"><a href="#" class="boton-datos-eliminar botones-datos" data-id="<?= $row['id_curso'] ?>">Eliminar</a></th>
-                        <th class="boton-dato"><a class="boton-datos-editar botones-datos" data-id="<?= $row['id_curso'] ?>" data-nombre="<?= $row['nombre']?>" data-capacidad="<?= $row['capacidad']?>" data-cupo="<?= $row['cupo_disponible']?>">Editar</a></th>
+                        <th class="boton-dato"><a class="boton-datos-editar botones-datos" data-id="<?= $row['id_curso'] ?>" data-nombre="<?= $row['nombre'] ?>" data-capacidad="<?= $row['capacidad'] ?>" data-cupo="<?= $row['cupo_disponible'] ?>">Editar</a></th>
                     </tr>
-            <?php endwhile; ?>
-        </table>
-    </div>
+                <?php endwhile; ?>
+            </table>
+        </div>
 
-    <div class="overlay" id="overlay">
-        <div class="confirmacion">
-            <h2>¿Estás seguro?</h2>
-            <p>Esta acción eliminará el registro de forma permanente.</p>
-            <div class="botones_confirmar">
-                <button class="btn btn-confirmar" id="confirmar" href="backend/functions/Cursos/delete.php?id=<?= $row['id_asignatura'] ?>">Eliminar</button>
-                <button class="btn btn-cancelar" id="cancelar">Cancelar</button>
+        <div class="overlay" id="overlay">
+            <div class="confirmacion">
+                <h2>¿Estás seguro?</h2>
+                <p>Esta acción eliminará el registro de forma permanente.</p>
+                <div class="botones_confirmar">
+                    <button class="btn btn-confirmar" id="confirmar" href="backend/functions/Cursos/delete.php?id=<?= $row['id_asignatura'] ?>">Eliminar</button>
+                    <button class="btn btn-cancelar" id="cancelar">Cancelar</button>
+                </div>
             </div>
         </div>
-    </div>
 
 
-    <div id="overlay-edit" class="overlay-edit">
-        <form action="\backend\functions\Cursos\edit.php" method="POST">
-            <h1>Registro de Asignaturas</h1>
-            <hr>
-            <div class="div-labels">
-                <input class="input-register" type="hidden" name="id_curso" id="id_edit">
-            </div>
-            <div class="editar-edit">
-                <label for="nombre" class="label">Nombre:</label>
-                <input type="text" name="nombre" id="name_edit" maxlength="20" minlength="3" required placeholder="Ingresa Nombre">
-                <label for="nombre" class="label">Capacidad:</label>
-                <input type="text" name="capacidad" id="capacidad_edit" maxlength="20" minlength="3" required placeholder="Ingresa Capacidad">
-                <label for="nombre" class="label">Cupos:</label>
-                <input type="text" name="cupos" id="cupos_edit" maxlength="20" minlength="3" required placeholder="Ingresa Cupos Disponibles">
-            </div>
-            <div>
-                <input type="submit" value="Actualizar Infomacion" id="actualizar"></input> 
-                <input type="button" value="Cancelar" id="cancelarEdit"></input>
-            </div>
-        </form>
-    </div>
+        <div id="overlay-edit" class="overlay-edit">
+            <form action="\backend\functions\Cursos\edit.php" method="POST">
+                <h1>Registro de Asignaturas</h1>
+                <hr>
+                <div class="div-labels">
+                    <input class="input-register" type="hidden" name="id_curso" id="id_edit">
+                </div>
+                <div class="editar-edit">
+                    <label for="nombre" class="label">Nombre:</label>
+                    <input type="text" name="nombre" id="name_edit" maxlength="20" minlength="3" required placeholder="Ingresa Nombre">
+                    <label for="nombre" class="label">Capacidad:</label>
+                    <input type="text" name="capacidad" id="capacidad_edit" maxlength="20" minlength="3" required placeholder="Ingresa Capacidad">
+                    <label for="nombre" class="label">Cupos:</label>
+                    <input type="text" name="cupos" id="cupos_edit" maxlength="20" minlength="3" required placeholder="Ingresa Cupos Disponibles">
+                </div>
+                <div>
+                    <input type="submit" value="Actualizar Infomacion" id="actualizar"></input>
+                    <input type="button" value="Cancelar" id="cancelarEdit"></input>
+                </div>
+            </form>
+        </div>
+    </main>
 
     <footer id="footer" class="footer">
         <p> &copy; <b> 2025 ITSP. Todos los derechos reservados </b></p>
