594ddbbe392c385869df37d8ccb8991c490e6994
Lucfontana <las.noches.luca@gmail.com>
2025-10-01 10:41:18 -0300

    registrar orientacion

---
+++ backend/functions/orientacion/orientacion_api.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once ('../../db/conexion.php');
+include_once ('orientacion_func.php');
+
+// Indicar que la respuesta de este PHP es un JSON
+header('Content-Type: application/json');
+
+$con = conectar_a_bd();
+
+if(isset($_POST['registrarOrientacion'])){
+    $nombre_asignatura = strip_tags(trim($_POST['nombreOrientacion']));
+
+    $existe = consultar_si_existe_orientacion($con, $nombre_asignatura);
+
+    $insertar_orientacion = insert_datos_orientacion($con, $existe, $nombre_asignatura);
+
+    echo json_encode($insertar_datos);
+
+}
+
+?>
