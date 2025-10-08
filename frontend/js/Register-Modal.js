fe9d0483598b3a6f8b8fdeb4af087814ec4a6c55
Lautaro Cardozo <velozpro450@gmail.com>
2025-10-08 09:36:08 -0300

    Register Cambiado y Arreglao

--- frontend/js/Register-Modal.js
+++ frontend/js/Register-Modal.js
@@ -1,50 +1,35 @@
 const botones = document.querySelectorAll(".btn");
-const dialogs = document.querySelectorAll("dialog");
 const closeBoton = document.querySelectorAll(".btn-Cerrar");
 const closeConfirm = document.querySelectorAll(".btn-enviar-registro");
 const inputs = document.querySelectorAll(".input-register");
+const overlays = document.querySelectorAll(".overlay");
 const body = document.getElementById("body-register");
 
 // Evita que el fondo se desplace cuando el modal está abierto
 
 botones.forEach((boton, index) => {
   boton.addEventListener("click", function () {
-    dialogs[index].showModal(); // Muestra el dialog correspondiente al botón
-    dialogs[index].style.transform = "translateY(5%)";
-    dialogs[index].style.opacity = "1";
-    dialogs[index].style.transition = "0.5s";
+    overlays[index].style.display = "flex"; // Muestra el overlay
+    overlays[index].style.opacity = "1";
+    overlays[index].style.transition = "0.5s";
     body.style.overflow = "hidden"; // Evita el desplazamiento del fondo
-    inputs.forEach(input => {
-      input.value = ""; // Limpia el valor del input
-    });
+    
   });
 })
 
 closeBoton.forEach((botonCerrar, index) => {
   botonCerrar.addEventListener("click", function () {
-    dialogs[index].style.transform = "translateY(-50%)";
-    dialogs[index].style.opacity = "0";
-    dialogs[index].style.transition = "0.5s";
+    overlays[index].style.opacity = "0"; // Muestra el overlay
+    overlays[index].style.transition = "0.5s"; // Muestra el overlay
     body.style.overflow = "visible"; // Evita el desplazamiento del fondo
 
     setTimeout(() => {
-      dialogs[index].close(); // Cierra el dialog correspondiente
+        overlays[index].style.display = "none";
     }, 500)
   });
 });
 
-closeConfirm.forEach((confirmCerrar, index) => {
-  confirmCerrar.addEventListener("click", function () {
-    dialogs[index].style.transform = "translateY(-50%)";
-    dialogs[index].style.opacity = "0";
-    dialogs[index].style.transition = "0.5s";
-    body.style.overflow = "visible"; // Evita el desplazamiento del fondo
 
-    setTimeout(() => {
-      dialogs[index].close(); // Cierra el dialog correspondiente
-    }, 500)
-  });
-});
 
 
 
