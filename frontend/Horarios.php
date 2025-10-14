1530d6e23c8402e7153ea82abecfa509ee7a8299
Lucfontana <las.noches.luca@gmail.com>
2025-10-14 20:35:22 -0300

    hola

--- frontend/Horarios.php
+++ frontend/Horarios.php
@@ -11,6 +11,7 @@ $sql3 = "SELECT * FROM espacios_fisicos";
 $query3 = mysqli_query($connect, $sql3);
 session_start();
 
+
 // Verificar si se ha enviado un ID de curso o de espacio a través de GET
 $cursosql = isset($_GET['curso_id']) ? intval($_GET['curso_id']) : 0;
 $espaciossql = isset($_GET['espacio_id']) ? intval($_GET['espacio_id']) : 0;
@@ -98,26 +99,33 @@ else {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Horarios</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
     <link rel="stylesheet" href="style/style.css">
-</head> 
-    <?php if (!isset($_SESSION['ci'])): ?> <?php include_once('error.php') ?> <?php else: ?>
-    <body> <!-- trae las barras de navegacion (sidebar y superior) --> <?php include 'nav.php'; ?> <main>
+</head>
+<?php if (!isset($_SESSION['ci'])): ?>
+    <?php include_once('error.php') ?>
+<?php elseif (isset($_SESSION['nivel_acceso'])): ?>
+
+    <body> <!-- trae las barras de navegacion (sidebar y superior) --> <?php include 'nav.php'; ?>
+        <main>
             <div id="contenido-mostrar-datos">
                 <h1>Horarios</h1>
-                <div class="filtros"> <label for="horario-select">Seleccionar Horario:</label> <select id="select-horarios" name="horario-select">
+                <div class="filtros"> <label for="horario-select">Seleccionar Horario:</label> <select id="select-horarios"
+                        name="horario-select">
                         <option value="1">Cursos</option>
                         <option value="2">Salones</option>
                     </select>
                     <div id="div-salones" style="display: none;">
                         <label for="Salones" id="select-salones">Seleccionar Salon:</label>
                         <select name="salones" id="salones-select" onchange="cambiarEspacio(this.value)">
-                        <option value="0">Seleccione un salón</option>
-                        <?php mysqli_data_seek($query3, 0); ?>
-                        <?php while ($row3 = mysqli_fetch_array($query3)): ?>
-                            <option value="<?= $row3['id_espacio'] ?>" <?= (isset($_GET['espacio_id']) && $_GET['espacio_id'] == $row3['id_espacio']) ? 'selected' : '' ?>><?= $row3['nombre'] ?></option>
-                        <?php endwhile; ?>
-                    </select>
+                            <option value="0">Seleccione un salón</option>
+                            <?php mysqli_data_seek($query3, 0); ?>
+                            <?php while ($row3 = mysqli_fetch_array($query3)): ?>
+                                <option value="<?= $row3['id_espacio'] ?>" <?= (isset($_GET['espacio_id']) && $_GET['espacio_id'] == $row3['id_espacio']) ? 'selected' : '' ?>><?= $row3['nombre'] ?>
+                                </option>
+                            <?php endwhile; ?>
+                        </select>
                     </div>
                     <div id="div-curso" style="display: block;">
                         <label for="Cursos" id="select-cursos">Seleccionar Curso:</label>
@@ -140,15 +148,16 @@ else {
                         <div class="dias">Viernes</div>
                     </div>
                 </div>
-                <?php if(isset($_GET['curso_id'])): ?>
-                <div class="datos-body"> <?php while ($row = mysqli_fetch_array($query)): ?> <div class="datos-row mostrar-datos">
-                            <div class="horas-dato"><?= $row['hora_inicio'] ?> - <?= $row['hora_final'] ?></div>
-                            <?php foreach ($dias as $dia): ?>
-                                <?php
+                <?php if (isset($_GET['curso_id'])): ?>
+                    <div class="datos-body"> <?php while ($row = mysqli_fetch_array($query)): ?>
+                            <div class="datos-row mostrar-datos">
+                                <div class="horas-dato"><?= $row['hora_inicio'] ?> - <?= $row['hora_final'] ?></div>
+                                <?php foreach ($dias as $dia): ?>
+                                    <?php
                                     $mostro = false;
                                     foreach ($materias_por_dia[$dia] as $m) {
                                         if ($m['hora_inicio'] == $row['hora_inicio']) {
-                                                echo "<div class='dia-dato'>
+                                            echo "<div class='dia-dato'>
                                                 <strong>{$m['nombre_asignatura']}</strong>
                                               </div>";
                                             $mostro = true;
@@ -157,18 +166,20 @@ else {
                                     if (!$mostro) {
                                         echo "<div class='dia-dato'><em>---</em></div>";
                                     }
-                                ?>
-                            <?php endforeach; ?>
-                        </div> <?php endwhile; ?> </div>
-                <?php elseif(isset($_GET['espacio_id'])): ?>
-                 <div class="datos-body"> <?php while ($row = mysqli_fetch_array($query)): ?> <div class="datos-row mostrar-datos">
-                            <div class="horas-dato"><?= $row['hora_inicio'] ?> - <?= $row['hora_final'] ?></div>
-                            <?php foreach ($dias as $dia): ?>
-                                <?php
+                                    ?>
+                                <?php endforeach; ?>
+                            </div> <?php endwhile; ?>
+                    </div>
+                <?php elseif (isset($_GET['espacio_id'])): ?>
+                    <div class="datos-body"> <?php while ($row = mysqli_fetch_array($query)): ?>
+                            <div class="datos-row mostrar-datos">
+                                <div class="horas-dato"><?= $row['hora_inicio'] ?> - <?= $row['hora_final'] ?></div>
+                                <?php foreach ($dias as $dia): ?>
+                                    <?php
                                     $mostro = false;
                                     foreach ($materias_por_dia[$dia] as $m) {
                                         if ($m['hora_inicio'] == $row['hora_inicio']) {
-                                                echo "<div class='dia-dato'>
+                                            echo "<div class='dia-dato'>
                                                 <strong>{$m['nombre_espacio']}</strong>
                                               </div>";
                                             $mostro = true;
@@ -177,15 +188,31 @@ else {
                                     if (!$mostro) {
                                         echo "<div class='dia-dato'><em>---</em></div>";
                                     }
-                                ?>
-                            <?php endforeach; ?>
-                        </div> <?php endwhile; ?> </div>
+                                    ?>
+                                <?php endforeach; ?>
+                            </div> <?php endwhile; ?>
+                    </div>
                 <?php endif; ?>
             </div>
         </main>
         <footer id="footer" class="footer">
             <p> &copy; <b> 2025 ITSP. Todos los derechos reservados </b></p>
-        </footer> <?php endif; ?> <!-- PARA HACER: ARREGLAR EL FOOTER QUE CON "ACTIVO" ANDA MAL -->
+        </footer>
+
+
+    <?php else:?>
+        <body>
+            
+            <?php include 'nav.php'; ?>
+
+            <main>
+                <div id="contenido-mostrar-datos">
+
+                </div>
+            </main>            
+        </body>
+
+    <?php endif; ?> <!-- PARA HACER: ARREGLAR EL FOOTER QUE CON "ACTIVO" ANDA MAL -->
     <script type="module" src="/frontend/js/confirm-espacios.js"></script>
     <script type="module" src="/frontend/js/prueba.js"></script>
     <script src="/backend/functions/Horarios/select-horarios.js"></script>
@@ -209,17 +236,17 @@ else {
 
         function cambiarEspacio(idEspacio) {
 
-            // Verifica que el idCurso exista y que no sea igual a "0"
+            // Verifica que el idEspacio exista y que no sea igual a "0"
             // (Por ejemplo, "0" podría representar la opción "Seleccionar curso" en un <select>)
             if (idEspacio && idEspacio !== "0") {
 
                 // Si la condición se cumple, redirige al usuario a la misma página
                 // pero agregando el parámetro 'curso_id' en la URL.
                 // Esto permite que el backend o PHP filtre los datos según el curso seleccionado.
-                window.location.href = "?espacio_id=" + idEspacio ;
+                window.location.href = "?espacio_id=" + idEspacio;
             }
         }
     </script>
-    </body>
+</body>
 
 </html>
--- frontend/nav.php
+++ frontend/nav.php
@@ -68,8 +68,6 @@ include_once 'functions.php';
             <a href="./Horarios.php">
                 <li data-tooltip="<?= t("aside_schedule") ?>"><img src="img/Iconos sidebar/calendario.png" alt="Calendario"></li>
             </a>
-            <a href="">
-
 
             <!-- Botón de idioma (solo visible en móvil) -->
             <li class="lang-icon" style="display:none;" data-tooltip="<?= t("aside_lang") ?>">
