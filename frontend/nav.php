b6a0740957afdc5e267caf338936bf3c99df095d
isisgz <isisgonzalez934@gmail.com>
2025-10-22 20:24:33 -0300

    modo oscuro funciona en todas las paginas (en breves hago q los que no tienen modo oscuro, tengan)

--- frontend/nav.php
+++ frontend/nav.php
@@ -103,7 +103,7 @@ include_once 'functions.php';
         integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
         crossorigin="anonymous"></script>
     <script src="js/confirm-logout.js"></script>
-    <!-- <script src="js/darkmode.js"></script> -->
+    <script src="js/darkmode.js"></script>
 
     <!-- Sweet alerts -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
