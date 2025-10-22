4bc495eb39d5f54e462dbc989ecf78ef93fde91d
isisgz <isisgonzalez934@gmail.com>
2025-10-22 12:34:23 -0300

    avances de darkmode
    darkmode comentados, colores cambiados, (no funca darkmode temporalmente), css darkmode temporal

--- frontend/js/darkmode.js
+++ frontend/js/darkmode.js
@@ -1,19 +1,20 @@
-const btnBienvenida = document.getElementsByClassName("btn-bienvenida");
-const elementos = document.querySelectorAll('.nav-link, .navbar-brand, .nav-item a, .navStyle, .nav-link.darkmode:hover, .navbar-brand.darkmode:hover, .navbar-toggler-icon');
-const navstyle = document.getElementById('nav');
-const aside = document.querySelector('aside');
-const darkmodeIcon = document.querySelector('#darkmode-icon');
-const footer = document.querySelector('footer');
+// const btnBienvenida = document.getElementsByClassName("btn-bienvenida");
+// const elementos = document.querySelectorAll('.nav-link, .navbar-brand, .nav-item a, .navStyle, .nav-link.darkmode:hover, .navbar-brand.darkmode:hover, .navbar-toggler-icon');
+// const navstyle = document.getElementById('nav');
+// const aside = document.querySelector('aside');
+// const darkmodeIcon = document.querySelector('#darkmode-icon');
+// const footer = document.querySelector('footer');
 
-darkmodeIcon.addEventListener('click', () => {
-    console.log('Dark mode activado'); // Verifica que el evento se dispare
-    Array.from(btnBienvenida).forEach(btnDienvenido => {
-        btnDienvenido.classList.toggle("darkmode");
-    });
-    Array.from(elementos).forEach(elemento => {
-        elemento.classList.toggle('darkmode');
-    });
-    navstyle.classList.toggle('darkmode');
-    aside.classList.toggle('darkmode');
-    footer.classList.toggle('darkmode');
-});
+// darkmodeIcon.addEventListener('click', () => {
+//     console.log('Dark mode activado'); // Verifica que el evento se dispare
+//     Array.from(btnBienvenida).forEach(btnDienvenido => {
+//         btnDienvenido.classList.toggle("darkmode");
+//     });
+//     Array.from(elementos).forEach(elemento => {
+//         elemento.classList.toggle('darkmode');
+//     });
+//     navstyle.classList.toggle('darkmode');
+//     aside.classList.toggle('darkmode');
+//     footer.classList.toggle('darkmode');
+// });
+
