5f7da0d493e5962f0d0e721d261d928b31a221d6
isisgz <isisgonzalez934@gmail.com>
2025-10-22 19:56:00 -0300

    preferencias de usuario y se mantiene el modo que elegis

--- frontend/js/darkmode.js
+++ frontend/js/darkmode.js
@@ -18,3 +18,36 @@
 //     footer.classList.toggle('darkmode');
 // });
 
+const d = document;
+const darkmodeIcon = d.querySelector('#darkmode-icon');
+
+let darkModeState = false;
+
+//media query detecta si el usuario tiene el modo oscuro activado
+const useDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+//cambiar a modo oscuro
+function toggleDarkMode(state) {
+    d.documentElement.classList.toggle('darkmode', state);
+    darkModeState = state;
+
+}
+
+//escuchar cambios en la preferencia del usuario
+function setDarkModeLocalStorage(state) {
+    localStorage.setItem('darkMode', state);
+}
+
+//configuracion inicial
+toggleDarkMode(useDark.matches);
+toggleDarkMode(localStorage.getItem('darkMode') == 'true');
+
+useDark.addListener((event) => toggleDarkMode(event.matches));
+
+//evento click en el icono y establece dentro del localstorage
+darkmodeIcon.addEventListener('click', () => {
+    darkModeState = !darkModeState;
+
+    toggleDarkMode(darkModeState);
+    setDarkModeLocalStorage(darkModeState);
+});
